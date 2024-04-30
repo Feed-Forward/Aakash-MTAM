faa969f0e56e80dc1bbe379dbb00831dd4ab1cf3
aakash0495 <aakash@mtamnyc.org>
2024-04-30 12:04:18 -0400

    React Code added

---
+++ feed/eslint.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    // Other ESLint configurations...
+    rules: {
+      // Other rules...
+      'no-unused-vars': 'off', // Disable the 'no-unused-vars' rule
+      'no-useless-concat': 'off', // Disable the 'no-useless-concat' rule
+    },
+  };
+  
